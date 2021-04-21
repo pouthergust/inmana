932222a55558becc8c0412dafb8a8304f26bc090
Gabriel Henrique <gabriel.ghvn@gmail.com>
2021-04-21 17:29:17 -0300

    Create and Send emails

---
+++ lib/inmana/supplies/get.ex
@@ -0,0 +1,16 @@
+defmodule Inmana.Supplies.Get do
+  alias Inmana.{Repo, Supply}
+
+  def call(uuid) do
+    case Repo.get(Supply, uuid) do
+      nil -> {:error, %{result: "Supply not found", status: :not_found}}
+      supply -> {:ok, supply}
+    end
+    # Supply
+    # |> Repo.get(uuid)
+    # |> handle_insert()
+  end
+
+  # defp handle_insert(%Supply{} = result), do: {:ok, result}
+  # defp handle_insert(nil), do: {:error, %{result: "Supply not found", status: :not_found}}
+end
